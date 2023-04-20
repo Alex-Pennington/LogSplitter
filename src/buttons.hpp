bd715ec9a6bdb5117484bba22f7773c76463c4e5
Alex-Pennington <53480806+Alex-Pennington@users.noreply.github.com>
2023-04-20 06:30:46 -0400

    Update 4/20

--- src/buttons.hpp
+++ src/buttons.hpp
@@ -1,17 +1,17 @@
 bool backlight_state = false;
 
 #define A_BUTTON_PIN PIN2
-#define B_BUTTON_PIN PIN3 
-#define C_BUTTON_PIN PIN4 
+#define B_BUTTON_PIN PIN3
+#define C_BUTTON_PIN PIN4
 #define D_BUTTON_PIN PIN5
-#define E_BUTTON_PIN PIN6 
-#define F_BUTTON_PIN PIN7 
-#define X_BUTTON_PIN 8 
+#define E_BUTTON_PIN PIN6
+#define F_BUTTON_PIN PIN7
+#define X_BUTTON_PIN 8
 
 bool button_A_state = false;
-//bool button_B_state = false;
+// bool button_B_state = false;
 bool button_C_state = false;
-//bool button_D_state = false;
+// bool button_D_state = false;
 bool button_E_state = false;
 bool button_F_state = false;
 bool button_X_state = false;
@@ -26,8 +26,9 @@ Bounce2::Button button_E = Bounce2::Button();
 Bounce2::Button button_F = Bounce2::Button();
 Bounce2::Button button_X = Bounce2::Button();
 
-void buttons() {
-  button_A.update();  
+void buttons()
+{
+  button_A.update();
   button_B.update();
   button_C.update();
   button_D.update();
@@ -37,46 +38,76 @@ void buttons() {
   // <Button>.pressed() RETURNS true IF THE STATE CHANGED
   // AND THE CURRENT STATE MATCHES <Button>.setPressedState(<HIGH or LOW>);
   // WHICH IS LOW IN THIS EXAMPLE AS SET WITH button.setPressedState(LOW); IN setup()
-  if ( button_A.changed() ) {
-     if (button_A.isPressed()) {
-      Serial.print("p");
-     } 
-  }
-  if ( button_B.changed() ) {
-    if (button_B.isPressed()) {
-      Serial.print("1");
-    } else {
-      //Serial.print("s");
+  if (button_A.changed())
+  {
+    if (button_A.isPressed())
+    {
+            Wire.beginTransmission(9); // transmit to device #9
+      Wire.write(2);             // sends x
+      Wire.endTransmission();
     }
   }
-if ( button_C.changed() ) {
-     if (button_C.isPressed()) {
+  if (button_B.changed())
+  {
+    if (button_B.isPressed())
+    {
+            Wire.beginTransmission(9); // transmit to device #9
+      Wire.write(2);             // sends x
+      Wire.endTransmission();
+    }
+    else
+    {
+    }
+  }
+  if (button_C.changed())
+  {
+    if (button_C.isPressed())
+    {
       Serial.print("s");
     }
   }
-  if ( button_D.changed() ) {
-        if (button_D.isPressed()) {
+  if (button_D.changed())
+  {
+    if (button_D.isPressed())
+    {
       Serial.print("0");
-    } else {
-      //Serial.println("s");
+    }
+    else
+    {
+      // Serial.println("s");
     }
   }
-  if ( button_E.changed() ) {
-     if (button_E.isPressed()) {
+  if (button_E.changed())
+  {
+    if (button_E.isPressed())
+    {
       Serial.println("on");
-     } else {
+    }
+    else
+    {
       Serial.println("off");
     }
-  }if ( button_F.changed() ) {
-     if (button_F.isPressed()) {
+  }
+  if (button_F.changed())
+  {
+    if (button_F.isPressed())
+    {
       backlight_state = !backlight_state;
     }
   }
-  if ( button_X.changed() ) {
-     if (button_X.isPressed()) {
-      Serial.println("X");
-     } else {
-      Serial.println("x");
+  if (button_X.changed())
+  {
+    if (button_X.isPressed())
+    {
+      Wire.beginTransmission(9); // transmit to device #9
+      Wire.write(1);             // sends x
+      Wire.endTransmission();    // stop transmitting
+    }
+    else
+    {
+      Wire.beginTransmission(9); // transmit to device #9
+      Wire.write(2);             // sends x
+      Wire.endTransmission();    // stop transmitting
     }
   }
 }
